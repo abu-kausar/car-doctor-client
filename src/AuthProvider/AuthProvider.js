14f8dd70d7ced830653490bb95be448612b2b6a7
abu-kausar <abukausar33@gmail.com>
2023-02-20 00:10:58 +0600

    updated with authentication

---
+++ src/AuthProvider/AuthProvider.js
@@ -0,0 +1,28 @@
+import React, { createContext, useState } from 'react';
+import {getAuth} from 'firebase/auth';
+import app from '../firebase/firebase.config';
+
+const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(null);
+
+    const createUser = (email) => {
+
+    }
+
+    const authInfo = {
+        user,
+        loading,
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,19 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderIdy,
-  appId: process.env.REACT_APP_appId
+  // apiKey: process.env.REACT_APP_apiKey,
+  // authDomain: process.env.REACT_APP_authDomain,
+  // projectId: process.env.REACT_APP_projectId,
+  // storageBucket: process.env.REACT_APP_storageBucket,
+  // messagingSenderId: process.env.REACT_APP_messagingSenderIdy,
+  // appId: process.env.REACT_APP_appId
+
+  apiKey: "AIzaSyDo_newDVDinGnwbS4_c8LNjbn_w2UG1ZI",
+  authDomain: "car-doctor-b3036.firebaseapp.com",
+  projectId: "car-doctor-b3036",
+  storageBucket: "car-doctor-b3036.appspot.com",
+  messagingSenderId: "225880799702",
+  appId: "1:225880799702:web:e5368df5d45941403bcc77"
 };
 
 // Initialize Firebase
